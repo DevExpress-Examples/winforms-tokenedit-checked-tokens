92d424827d564fc89bea19d65854b1151f247a4c
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@devexpress.com>
2021-11-22 16:49:02 +0300

    Fix painting issues

--- CS/TokenEdit_Glyph_InEndOfToken/CustomTokenEdit/CustomTokenEditPainter.cs
+++ CS/TokenEdit_Glyph_InEndOfToken/CustomTokenEdit/CustomTokenEditPainter.cs
@@ -9,38 +9,40 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 
-namespace TokenEdit_Glyph_InEndOfToken.CustomTokenEdit
-{
-    public class CustomTokenEditPainter : TokenEditPainter
-    {
+namespace TokenEdit_Glyph_InEndOfToken.CustomTokenEdit {
+    public class CustomTokenEditPainter : TokenEditPainter {
         CustomTokenEdit editor;
-        public void SetEditor(CustomTokenEdit _editor)
-        {
+        public void SetEditor(CustomTokenEdit _editor) {
             editor = _editor;
         }
 
-        protected override void DoDrawGlyph(ControlGraphicsInfoArgs info, TokenEditTokenInfo tokenInfo)
-        {
+        protected override void DoDrawGlyph(ControlGraphicsInfoArgs info, TokenEditTokenInfo tokenInfo) {
             base.DoDrawGlyph(info, tokenInfo);
-            if (editor.Properties.CheckBoxCollection.Images.Count != 0)
-            {
+
+            if (editor.Properties.CheckBoxSkinElement != null) {
                 int index = 0;
                 CustomTokenEditTokenInfo customTokenInfo = tokenInfo as CustomTokenEditTokenInfo;
                 int offset = customTokenInfo.Checked == true ? 4 : 0;
-                switch (customTokenInfo.CheckBoxState)
-                    {
-                        case ObjectState.Normal:
-                            index = offset;
-                            break;
-                        case ObjectState.Hot:
-                            index = offset + 1;
-                            break;
-                        case ObjectState.Hot | ObjectState.Pressed:
-                            index = offset + 2;
-                            break;
-                    }
                 Rectangle rect = customTokenInfo.CheckBoxGlyphBounds;
-                info.Graphics.DrawImage(editor.Properties.CheckBoxCollection.Images[index], new Rectangle(rect.X + (int)info.Graphics.ClipBounds.X, rect.Y + (int)info.Graphics.ClipBounds.Y, rect.Width, rect.Height));
+                Rectangle r = new Rectangle(rect.X + (int)info.Graphics.ClipBounds.X, rect.Y + (int)info.Graphics.ClipBounds.Y, rect.Width, rect.Height);
+                SkinElementInfo skinElementInfo = new SkinElementInfo(editor.Properties.CheckBoxSkinElement, r);
+                switch (customTokenInfo.CheckBoxState) {
+                    case ObjectState.Normal:
+                        index = offset;
+                        break;
+                    case ObjectState.Hot:
+                        index = offset + 1;
+                        break;
+                    case ObjectState.Hot | ObjectState.Pressed:
+                        index = offset + 2;
+                        break;
+                }
+                skinElementInfo.ImageIndex = index;
+
+
+                ImageCollection imageCollection = editor.Properties.CheckBoxSkinElement.Image.GetImages();
+                info.Graphics.DrawImage(imageCollection.Images[index], new Rectangle(rect.X + (int)info.Graphics.ClipBounds.X, rect.Y + (int)info.Graphics.ClipBounds.Y, rect.Width, rect.Height));
+                ObjectPainter.DrawObject(info.Cache, SkinElementPainter.Default, skinElementInfo);
             }
         }
     }
--- CS/TokenEdit_Glyph_InEndOfToken/CustomTokenEdit/RepositoryItemCustomTokenEdit.cs
+++ CS/TokenEdit_Glyph_InEndOfToken/CustomTokenEdit/RepositoryItemCustomTokenEdit.cs
@@ -7,42 +7,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace TokenEdit_Glyph_InEndOfToken.CustomTokenEdit
-{
+namespace TokenEdit_Glyph_InEndOfToken.CustomTokenEdit {
     [UserRepositoryItem("Register")]
-    public class RepositoryItemCustomTokenEdit : RepositoryItemTokenEdit
-    {
-        static RepositoryItemCustomTokenEdit() {  Register(); }
+    public class RepositoryItemCustomTokenEdit : RepositoryItemTokenEdit {
+        static RepositoryItemCustomTokenEdit() { Register(); }
         public RepositoryItemCustomTokenEdit() { checkBoxCollection = GetCheckBoxImages(); }
 
         internal const string EditorName = "CustomTokenEdit";
-        public static void Register()
-        {
+        public static void Register() {
             EditorRegistrationInfo.Default.Editors.Add(new EditorClassInfo(EditorName, typeof(CustomTokenEdit),
                 typeof(RepositoryItemCustomTokenEdit), typeof(CustomTokenEditViewInfo),
                 new CustomTokenEditPainter(), true, null));
         }
         public override string EditorTypeName { get { return EditorName; } }
 
-        readonly ImageCollection checkBoxCollection;
-        public ImageCollection CheckBoxCollection{ get { return checkBoxCollection; }  }
+        readonly SkinElement checkBoxCollection;
+        public SkinElement CheckBoxSkinElement { get { return checkBoxCollection; } }
 
-        protected virtual ImageCollection GetCheckBoxImages()
-        {
+        protected virtual SkinElement GetCheckBoxImages() {
             Skin skin = EditorsSkins.GetSkin(DevExpress.LookAndFeel.UserLookAndFeel.Default);
             SkinElement skinElement = skin["CheckBox"];
             if (skinElement == null)
                 return null;
-            return skinElement.Image.GetImages();
+            return skinElement;
         }
 
-        protected override TokenEditCheckedItemCollection CreateCheckedItemCollection()
-        {
+        protected override TokenEditCheckedItemCollection CreateCheckedItemCollection() {
             return new CustomTokenEditCheckedItemCollection(this);
         }
 
-        protected override TokenEditSelectedItemCollection CreateSelectedItemCollection()
-        {
+        protected override TokenEditSelectedItemCollection CreateSelectedItemCollection() {
             return new CustomTokenEditSelectedItemCollection(this);
         }
 
@@ -52,24 +46,21 @@ namespace TokenEdit_Glyph_InEndOfToken.CustomTokenEdit
         }
 
         public event EventHandler<TokenCheckedChangedEventArgs> TokenCheckedChanged;
-        protected internal void OnTokenCheckedChanged(CustomTokenEditToken token, bool repaint)
-        {
+        protected internal void OnTokenCheckedChanged(CustomTokenEditToken token, bool repaint) {
             RepaintEditIfRequired(repaint);
             RaiseTokenCheckedChanged(token);
         }
 
-        private void RaiseTokenCheckedChanged(CustomTokenEditToken token)
-        {
+        private void RaiseTokenCheckedChanged(CustomTokenEditToken token) {
             EventHandler<TokenCheckedChangedEventArgs> handler = TokenCheckedChanged;
             if (handler != null)
                 handler(this, new TokenCheckedChangedEventArgs(token));
         }
 
-        private void RepaintEditIfRequired(bool repaint)
-        {
+        private void RepaintEditIfRequired(bool repaint) {
             CustomTokenEdit edit = OwnerEdit as CustomTokenEdit;
             if (repaint && edit != null)
                 edit.Repaint();
-        }        
+        }
     }
 }
